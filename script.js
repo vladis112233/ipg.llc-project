40927f7650284deb7ce6b58d25fa41daef7667d2
vladis112233 <124692818+vladis112233@users.noreply.github.com>
2023-11-21 11:44:58 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function (){
+    $('.slider').slick({
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 450,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
